103e209f9e836d964414cede72b2dc4fee8f7f14
Ismail Aidar <ismailaidar@gmail.com>
2021-03-22 13:13:29 -0400

    Update AuthorCollectionsController.cs

--- CourseLibrary.API/Controllers/AuthorCollectionsController.cs
+++ CourseLibrary.API/Controllers/AuthorCollectionsController.cs
@@ -16,7 +16,7 @@ namespace CourseLibrary.API.Controllers
     {
         private readonly ICourseLibraryRepository _courseLibraryRepository;
         private readonly IMapper _mapper;
-        //this is a test
+        //this is a test and another
         public AuthorCollectionsController(ICourseLibraryRepository courseLibraryRepository,
             IMapper mapper)
         {
